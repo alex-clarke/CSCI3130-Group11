7bc10a7a376de0a813c917372faf376f9755d19e
JayParsons <jason.parsons@dal.ca>
2017-06-06 13:07:01 -0300

    Jason and Kyle created unit tests to test the temperature range as well as the measured temperature.

--- app/src/test/java/com/csci3130_group11/csci3130_group11/TemperatureTests.java
+++ app/src/test/java/com/csci3130_group11/csci3130_group11/TemperatureTests.java
@@ -6,29 +6,28 @@ package com.csci3130_group11.csci3130_group11;
  */
 
 import org.junit.Test;
-//import csci3130_group11.csci3130_group11.MainActivity.*;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-
 import static org.junit.Assert.*;
-
 
 public class TemperatureTests {  //Low:-40  High:125
     @Test
     public void withinInputtedRange() throws Exception {
-        assertEquals(Temperature.checkMeasurement(-50, 130), false);
-        assertEquals(Temperature.checkMeasurement(-50, 40), false);
-        assertEquals(Temperature.checkMeasurement(-20, 130), false);
-        assertEquals(Temperature.checkMeasurement(20, 40), true);
-        assertEquals(Temperature.checkMeasurement(-40, -125), true);
+        Temperature t = new Temperature();
+        assertEquals(t.checkMeasurement(-50, 130), false);
+        assertEquals(t.checkMeasurement(-50, 40), false);
+        assertEquals(t.checkMeasurement(-20, 130), false);
+        assertEquals(t.checkMeasurement(20, 40), true);
+        assertEquals(t.checkMeasurement(-40, -125), true);
     }
 
     @Test
     public void withinMeasurement() throws Exception {
-        assertEquals(Temperature.checkUserInputtedRange(0), false);
-        assertEquals(Temperature.checkUserInputtedRange(40), false);
-        assertEquals(Temperature.checkUserInputtedRange(20), true);
+        Temperature t = new Temperature();
+        assertEquals(t.checkUserInputtedRange(0), false);
+        assertEquals(t.checkUserInputtedRange(40), false);
+        assertEquals(t.checkUserInputtedRange(20), true);
     }
 }
