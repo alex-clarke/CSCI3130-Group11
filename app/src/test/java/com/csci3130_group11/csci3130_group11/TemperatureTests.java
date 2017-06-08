97f8f76896213716eaaa525f128274eea0d7e3ee
aclarke <al216364@dal.ca>
2017-06-08 12:25:53 -0300

    Integrated Temperature and Light Tests - AClarke

--- app/src/test/java/com/csci3130_group11/csci3130_group11/TemperatureTests.java
+++ app/src/test/java/com/csci3130_group11/csci3130_group11/TemperatureTests.java
@@ -6,28 +6,25 @@ package com.csci3130_group11.csci3130_group11;
  */
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TemperatureTests {  //Low:-40  High:125
     @Test
     public void withinInputtedRange() throws Exception {
         Temperature t = new Temperature();
-        assertEquals(t.checkMeasurement(-50, 130), false);
-        assertEquals(t.checkMeasurement(-50, 40), false);
-        assertEquals(t.checkMeasurement(-20, 130), false);
-        assertEquals(t.checkMeasurement(20, 40), true);
-        assertEquals(t.checkMeasurement(-40, -125), true);
+        assertEquals(t.checkUserInputtedRange(-50, 130), false);
+        assertEquals(t.checkUserInputtedRange(-50, 40), false);
+        assertEquals(t.checkUserInputtedRange(-20, 130), false);
+        assertEquals(t.checkUserInputtedRange(20, 40), true);
+        assertEquals(t.checkUserInputtedRange(-40, -125), true);
     }
 
     @Test
     public void withinMeasurement() throws Exception {
         Temperature t = new Temperature();
-        assertEquals(t.checkUserInputtedRange(0), false);
-        assertEquals(t.checkUserInputtedRange(40), false);
-        assertEquals(t.checkUserInputtedRange(20), true);
+        assertEquals(t.checkMeasurement(0), false);
+        assertEquals(t.checkMeasurement(40), false);
+        assertEquals(t.checkMeasurement(20), true);
     }
 }
