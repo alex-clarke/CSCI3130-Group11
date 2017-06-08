30cd50f5763107d134ed0a9f8a419a2c8f3308ee
xhc2081 <xhc2081@163.com>
2017-06-08 11:59:12 -0300

    setranges

--- app/src/main/java/com/csci3130_group11/csci3130_group11/SetRanges.java
+++ app/src/main/java/com/csci3130_group11/csci3130_group11/SetRanges.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class  SetRanges extends AppCompatActivity implements View.OnClickListener {
 
     private EditText humidityMax;
     private EditText humidityMin;
@@ -53,59 +53,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.increase:
                 if (humidityMax.isFocused()) {
                     if (humidityMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double humidityMaxValue = Double.parseDouble(humidityMax.getText().toString());
                     if(Util.increase(humidityMaxValue)>100) {
-                        Toast.makeText(MainActivity.this, "Humidity cannot be more than 100% !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Humidity cannot be more than 100% !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     humidityMax.setText(Util.increase(humidityMaxValue) + "");
                 } else if (humidityMin.isFocused()) {
                     if (humidityMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double humidityMinValue = Double.parseDouble(humidityMin.getText().toString());
                     if(Util.increase(humidityMinValue)>100) {
-                        Toast.makeText(MainActivity.this, "Humidity cannot be more than 100% !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Humidity cannot be more than 100% !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     humidityMin.setText(Util.increase(humidityMinValue) + "");
                 } else if (tempMax.isFocused()) {
                     if (tempMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double tempMaxValue = Double.parseDouble(tempMax.getText().toString());
                     tempMax.setText(Util.increase(tempMaxValue) + "");
                 } else if (tempMin.isFocused()) {
                     if (tempMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double tempMinValue = Double.parseDouble(tempMin.getText().toString());
                     tempMin.setText(Util.increase(tempMinValue) + "");
                 } else if (lightMax.isFocused()) {
                     if (lightMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double lightMaxValue = Double.parseDouble(lightMax.getText().toString());
                     if(Util.increase(lightMaxValue)>100) {
-                        Toast.makeText(MainActivity.this, "Light cannot be more than 100% !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Light cannot be more than 100% !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     lightMax.setText(Util.increase(lightMaxValue) + "");
                 } else if (lightMin.isFocused()) {
                     if (lightMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double lightMinValue = Double.parseDouble(lightMin.getText().toString());
                     if(Util.increase(lightMinValue)>100) {
-                        Toast.makeText(MainActivity.this, "Light cannot be more than 100% !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Light cannot be more than 100% !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     lightMin.setText(Util.increase(lightMinValue) + "");
@@ -114,60 +114,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.decrease:
                 if (humidityMax.isFocused()) {
                     if (humidityMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double humidityMaxValue = Double.parseDouble(humidityMax.getText().toString());
                     if (Util.decrease(humidityMaxValue) < 0) {
-                        Toast.makeText(MainActivity.this, "Humidity cannot be less than 0 !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Humidity cannot be less than 0 !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     humidityMax.setText(Util.decrease(humidityMaxValue) + "");
 
                 } else if (humidityMin.isFocused()) {
                     if (humidityMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double humidityMinValue = Double.parseDouble(humidityMin.getText().toString());
                     if (Util.decrease(humidityMinValue) < 0) {
-                        Toast.makeText(MainActivity.this, "Humidity cannot be less than 0 !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Humidity cannot be less than 0 !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     humidityMin.setText(Util.decrease(humidityMinValue) + "");
                 } else if (tempMax.isFocused()) {
                     if (tempMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double tempMaxValue = Double.parseDouble(tempMax.getText().toString());
                     tempMax.setText(Util.decrease(tempMaxValue) + "");
                 } else if (tempMin.isFocused()) {
                     if (tempMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double tempMinValue = Double.parseDouble(tempMin.getText().toString());
                     tempMin.setText(Util.decrease(tempMinValue) + "");
                 } else if (lightMax.isFocused()) {
                     if (lightMax.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double lightMaxValue = Double.parseDouble(lightMax.getText().toString());
                     if (Util.decrease(lightMaxValue)  < 0) {
-                        Toast.makeText(MainActivity.this, "Light cannot be less than 0 !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Light cannot be less than 0 !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     lightMax.setText(Util.decrease(lightMaxValue) + "");
                 } else if (lightMin.isFocused()) {
                     if (lightMin.getText().toString().trim().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "Please input!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Please input!", Toast.LENGTH_SHORT).show();
                         break;
                     }
                     double lightMinValue = Double.parseDouble(lightMin.getText().toString());
                     if (Util.decrease(lightMinValue) < 0) {
-                        Toast.makeText(MainActivity.this, "Light cannot be less than 0 !", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SetRanges.this, "Light cannot be less than 0 !", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     lightMin.setText(Util.decrease(lightMinValue) + "");
